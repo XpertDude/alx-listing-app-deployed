2482c70b3e6525c0a6f398183a1e3b4f96a2b29b
XpertDude <braodby@gmail.com>
2025-07-22 13:54:29 +0100

    fix directory issue

---
+++ interfaces/index.ts
@@ -0,0 +1,32 @@
+// interfaces/index.ts
+
+export interface ButtonProps {
+    title: string;
+    styles: string;
+}
+
+export interface CardProps {
+    image: string;
+    name: string;
+    price: number;
+    rating: number;
+}
+
+export interface PropertyProps {
+    name: string;
+    address: {
+        state: string;
+        city: string;
+        country: string;
+    };
+    rating: number;
+        category: string[];
+        price: number
+        offers: {
+            bed: string;
+            shower: string;
+            occupants: string;
+        },
+        image: string;
+        discount: string
+}
